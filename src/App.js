d1e026aeffa78e9b41627035ac12d4d9df8c782e
Rodrigo Dourado <dourado.rodrigo@outlook.com>
2022-11-14 17:16:05 -0300

    Added 2 pages. Added functionality to btn's by Link react dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Home from "./Pages/Home/Home";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./Pages/Register/Register";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path='/' element={<Home/>}> */}
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Hero.css";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <div className="container">
-      <btn className="btn btn-newUser">Primeira vez aqui</btn>
-      <btn className="btn btn-primary">Ja conheço</btn>
+      <Link to="/Register">
+        <btn className="btn btn-newUser">Primeira vez aqui</btn>
+      </Link>
+      <Link to="/Login">
+        <btn className="btn btn-primary">Já conheço!</btn>
+      </Link>
     </div>
   );
 };
